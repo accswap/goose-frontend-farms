298994ef4fc444a23669a2262aacab2389efd5aa
Cryptocode <crytocode99@gmail.com>
2021-05-04 10:01:15 +0700

    initial commit

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,31 +1,42 @@
 import React, { useContext } from 'react'
-import { useWallet } from '@binance-chain/bsc-use-wallet'
-import { allLanguages } from 'config/localisation/languageCodes'
-import { LanguageContext } from 'contexts/Localisation/languageContext'
+import { Menu as UikitMenu, ConnectorId } from '@pancakeswap-libs/uikit'
+import { useWeb3React } from '@web3-react/core'
+import { allLanguages } from 'constants/localisation/languageCodes'
+import { LanguageContext } from 'hooks/LanguageContext'
 import useTheme from 'hooks/useTheme'
-import { usePriceCakeBusd } from 'state/hooks'
-import { Menu as UikitMenu } from '@pancakeswap-libs/uikit'
-import config from './config'
+import useGetPriceData from 'hooks/useGetPriceData'
+import { injected, bsc, walletconnect } from 'connectors'
+import links from './config'
 
-const Menu = (props) => {
-  const { account, connect, reset } = useWallet()
+const Menu: React.FC = props => {
+  const { account, activate, deactivate } = useWeb3React()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
-  const cakePriceUsd = usePriceCakeBusd()
+  const cakePriceUsd = useGetPriceData()
 
   return (
     <UikitMenu
-      account={account}
-      login={connect}
-      logout={reset}
+      links={links}
+      priceLink="https://www.coingecko.com/en/coins/goose-finance"
+      account={account as string}
+      login={(connectorId: ConnectorId) => {
+        if (connectorId === 'walletconnect') {
+          return activate(walletconnect)
+        }
+
+        if (connectorId === 'bsc') {
+          return activate(bsc)
+        }
+
+        return activate(injected)
+      }}
+      logout={deactivate}
       isDark={isDark}
       toggleTheme={toggleTheme}
-      currentLang={selectedLanguage && selectedLanguage.code}
+      currentLang={selectedLanguage?.code || ''}
       langs={allLanguages}
       setLang={setSelectedLanguage}
-      cakePriceUsd={cakePriceUsd.toNumber()}
-      links={config}
-      priceLink="https://www.coingecko.com/en/coins/goose-finance"
+      cakePriceUsd={cakePriceUsd}
       {...props}
     />
   )
