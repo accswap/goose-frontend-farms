1af3c2a3b834a26dfbf103b7b6a80ec54c1d7caf
Cryptocode <crytocode99@gmail.com>
2021-05-08 03:01:37 +0700

    initial

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -81,7 +81,7 @@ const config: MenuEntry[] = [
       },
       {
         label: 'Blog',
-        href: 'https://gadexswap.medium.com/',
+        href: 'https://gadex.medium.com/',
       },
     ],
   },
